7ac3bc33390c7f4dd46245284e2515057679583c
Avishek3223 <avishekmishra56@gmail.com>
2023-05-15 15:45:23 +0530

    .

--- project2.js
+++ project2.js
@@ -3,8 +3,24 @@
 var txtInput = document.querySelector("#txtInput");
 var txtOutput = document.querySelector("#txtOutput")
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+function getTranslationURL(text){
+  return serverURL + "?" + "text=" +text
+}
+
 function clickHandler(){
-  txtOutput.innerText = txtInput.value;
+  // txtOutput.innerText = txtInput.value;
+
+  var inputtext= txtInput.value;
+  fetch(getTranslationURL(inputtext))
+  .then(response => response.json())
+  .then(json => {
+       var translatedText = json.contents.translatedText;
+       txtOutput.innerText = translatedText; 
+  })
+
 }
 
 btnTranslate.addEventListener("click", clickHandler)
